5522573f3bb34cad2639584eb85e3de5a6117367
joao <joao.ax1996@aluno.ifsc.edu.br>
2026-03-26 17:33:31 -0300

    Fila iniciada. WIP

--- libprg/src/include/libprg/libprg.h
+++ libprg/src/include/libprg/libprg.h
@@ -1,10 +1,20 @@
 #ifndef LIBPRG_LIBPRG_H
 #define LIBPRG_LIBPRG_H
+#include <stdbool.h>
 
- /*---    PILHA   ---*/
-    typedef struct pilha_t pilha_t;
+/*---    PILHA   ---*/
+    typedef struct pilha pilha_t;
      pilha_t* criar_pilha(int capacidade);
     int empilhar(pilha_t* pilha, int valor);
     int desempilhar (pilha_t* pilha);
+    int tamanho (pilha_t* pilha);
+    bool vazia (pilha_t* pilha);
+    int destroy_pilha (pilha_t* pilha);
+
+ /*---   FILA   ---*/
+    typedef struct fila fila_t;
+    fila_t* criar_fila (int capacidade);
+    void enfileirar (fila_t* fila, int valor);
+    int destruir_fila (fila_t* fila);
 
 #endif
